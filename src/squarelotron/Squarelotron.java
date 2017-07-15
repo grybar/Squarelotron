b5d809c6b4d881cd1af2d2ea2026d739c19a4609
Garrett <gmrybarczyk@gmail.com>
2017-07-15 13:46:30 -0600

    Added constructor for squarelotron class

--- src/squarelotron/Squarelotron.java
+++ src/squarelotron/Squarelotron.java
@@ -3,4 +3,15 @@ package squarelotron;
 public class Squarelotron {
 	int size;
 	int[][] squarelotron;
+	
+	public Squarelotron(int n) {
+		size = n;
+		squarelotron = new int[n][n];
+		
+		for (int i = 0;i < n; i++) {
+			for (int j = 0;j < n; j++) {
+				squarelotron[i][j] = i * j;
+			}
+		}
+	}
 }
